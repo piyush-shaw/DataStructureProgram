529ad2163aabddd75e7f23c4043260bf3b3d1123
piyush-shaw <piyushshaw0000@gmail.com>
2023-02-21 13:13:53 +0530

    [Piyush] added to take an arithmetic expression and show it balanced or not

--- DataStructure/DataStructure/Program.cs
+++ DataStructure/DataStructure/Program.cs
@@ -7,7 +7,7 @@ internal class Program
         Console.WriteLine("Welcome to Data Structure Program ");
         while (true)
         {
-            Console.WriteLine("\n1.Unordered List \n2.Ordered List");
+            Console.WriteLine("\n1.Unordered List \n2.Ordered List \n3.Balancing Parantheses");
             Console.WriteLine("Enter a choice from above");
             int check = Convert.ToInt32(Console.ReadLine());
             switch (check)
@@ -17,6 +17,19 @@ internal class Program
                     break;
                 case 2:
                     OrderedList.Ordered();
+                    break;
+                case 3:
+                    char[] expression = "(5+6)∗(7+8)/(4+3)(5+6)∗(7+8)/(4+3)".ToCharArray();
+                    BalancingParentheses.StackOperations stackOperations = new BalancingParentheses.StackOperations();
+                    bool result = stackOperations.Isbalanced(expression);
+                    if (result)
+                    {
+                        Console.WriteLine("Parantheses Balanced");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Parantheses not Balanced");
+                    }
                     break;
                 default:
                     Console.WriteLine("Please enter the correct choice ");
